7e082ccb4876c47ebd195a0d61bdaf06966f1403
MrQin877 <kuanchinzhong@gmail.com>
2025-09-17 00:51:33 +0800

    File structure modify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import HeaderNav from "./component/header";
 
 const link = ({ isActive }) => ({ marginRight: 12, fontWeight: isActive ? 700 : 400 });
 
 export default function App() {
   return (
+    <>
+    <HeaderNav/>
     <BrowserRouter basename="/IYO_SMART_PANTRY/frontend/src/pages">
       <nav style={{ padding: 12, borderBottom: "1px solid #eee" }}>
         <NavLink to="/" end style={link}>Home</NavLink>
@@ -16,5 +19,6 @@ export default function App() {
         <Route path="/about" element={<About/>} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
