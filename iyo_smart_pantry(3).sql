02677c025a242e705305313453c84da70e5d3e34
MrQin877 <kuanchinzhong@gmail.com>
2025-10-13 04:43:06 +0800

    Update iyo_smart_pantry(3).sql

--- iyo_smart_pantry(3).sql
+++ iyo_smart_pantry(3).sql
@@ -17,6 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `iyo_smart_pantry` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `iyo_smart_pantry`;
+
 --
 -- Database: `iyo_smart_pantry`
 --
