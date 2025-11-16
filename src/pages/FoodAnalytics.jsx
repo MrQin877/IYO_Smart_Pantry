97bef53629d15baf4be0c889263e9b965d2778de
Elain0417 <elainliow@gmail.com>
2025-11-16 19:21:31 +0800

    Improve pie chart percentage accuracy
    - show decimals with two decimal point in the pie chart

--- src/pages/FoodAnalytics.jsx
+++ src/pages/FoodAnalytics.jsx
@@ -772,7 +772,7 @@ const FoodAnalytics = () => {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+                  label={({ name, percent }) => `${name} ${(percent * 100).toFixed(2)}%`}
                   outerRadius={100}
                   fill="#8884d8"
                   dataKey="value"
